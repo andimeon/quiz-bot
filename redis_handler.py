137625b78d52d74d603225b33dfe7a9f6607dfaf
andimeon <andimeon@gmail.com>
2020-08-14 16:59:19 +0900

    update score handler

--- redis_handler.py
+++ redis_handler.py
@@ -9,8 +9,6 @@ load_dotenv()
 
 class RedisHandler:
     question, answer, offset, fail, comment = '', '', '', '', ''
-    question_number, user = '', ''
-    score = 0
 
     def __init__(self):
         self.redis = redis.Redis(
@@ -21,16 +19,13 @@ class RedisHandler:
             decode_responses=True,
         )
 
-    def initiate_score(self, user):
-        self.user = user
-        if self.redis.get(user):
-            user_info_str = self.redis.get(user)
-            user_info = json.loads(user_info_str)
-            self.score = user_info['score']
-    
-    def initiate_new_quiz_block(self):
-        self.question_number = self.redis.randomkey()
-        redis_value = self.redis.get(self.question_number)
+    def initiate_score(self, score_id):
+        if self.redis.get(score_id) is None:
+            self.redis.set(score_id, 0)
+              
+    def initiate_new_quiz_block(self, user):
+        question_number = self.redis.randomkey()
+        redis_value = self.redis.get(question_number)
         quiz_block = json.loads(redis_value)
 
         self.question = quiz_block['question']
@@ -38,11 +33,16 @@ class RedisHandler:
         self.offset = quiz_block['offset']
         self.fail = quiz_block['fail']
         self.comment = quiz_block['comment']
-    
-    def set_score(self):
-        user_info = json.dumps({
-            'last_asked_question': self.question_number,
-            'score': self.score})
 
-        self.redis.set(self.user, user_info)
+        user_info = json.dumps({'last_asked_question': question_number})
+        self.redis.set(user, user_info)
+        
+    def set_score(self, score_id, score=0):
+        self.redis.set(score_id, (int(self.redis.get(score_id)) + score))
+    
+    def get_score(self, score_id):
+        if self.redis.get(score_id):
+            return self.redis.get(score_id)
+        else:
+            return 0
         
